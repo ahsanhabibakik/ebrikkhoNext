257d5ef3a4b3815dcef979deebe8a875095e1a87
Syed Mir Habib <mirakik.habib@gmail.com>
2025-04-20 20:08:06 +0600

    added some more items

--- src/lib/products.js
+++ src/lib/products.js
@@ -1,9 +1,19 @@
-import React from 'react'
-
-export default function products() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+// lib/products.ts
+export const products = [
+  {
+    id: "1",
+    name: "Snake Plant",
+    slug: "snake-plant",
+    price: 250,
+    image: "/images/snake.jpg",
+    description: "Low-maintenance indoor plant.",
+  },
+  {
+    id: "2",
+    name: "Aloe Vera",
+    slug: "aloe-vera",
+    price: 180,
+    image: "/images/aloe.jpg",
+    description: "Medicinal and decorative.",
+  },
+];
