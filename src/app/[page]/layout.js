69664318ed741f4ca9721715b917570ad182e3fe
Syed Mir Habib <mirakik.habib@gmail.com>
2025-04-28 04:57:58 +0600

    Add layout.js with initial metadata, viewport, and themeColor exports

---
+++ src/app/[page]/layout.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "Page Title",
+  description: "Page description",
+};
+
+export const viewport = "width=device-width, initial-scale=1";
+export const themeColor = "#ffffff";
