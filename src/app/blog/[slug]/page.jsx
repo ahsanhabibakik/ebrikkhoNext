de1b8d2f5ae10d298e17db87268b315042ba0299
Syed Mir Habib <mirakik.habib@gmail.com>
2025-04-28 04:51:22 +0600

    Remove unused Link import from BlogPostPage component

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -12,7 +12,6 @@ import {
   Link,
 } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function BlogPostPage({ params }) {
   const resolvedParams = use(params);
